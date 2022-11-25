6b505ade12e9f5536bb810758c3b074ee23229f5
Extralazon <dextralazon@gmail.com>
2022-11-25 20:28:08 -0300

    alterado nome de Main para Poker no texto

--- src/Rodadas.java
+++ src/Rodadas.java
@@ -12,13 +12,13 @@ public class Rodadas {
 
         //defino bet para todos os jogadores
         jogadoresQueApostaram = new String[jogadores];
-        for (int i=0; i<jogadoresQueApostaram.length; i++){
+        for (int i = 0; i < jogadoresQueApostaram.length; i++) {
             jogadoresQueApostaram[i] = "bet";
         }
 
         //criar variaveis necessarias
         Object[][] decisaoJogador = new Object[jogadores][2];
-        int somaApostas=0;
+        int somaApostas = 0;
         int opcaoJogador;
         int apostaDaRodada = 0;
         boolean apostasIguais = false;
@@ -32,41 +32,55 @@ public class Rodadas {
                     throw new ArithmeticException("opção inválida");
                 } else if (opcaoJogador == 1) {
                     decisaoJogador[i][0] = "bet";
-                    System.out.println("Jogador escolheu bet. Qual o valor da aposta? (valor da aposta atual: " + apostaDaRodada);
+                    System.out.println("Jogador " + (i + 1) + " escolheu bet.");
+                    System.out.println("Qual o valor da aposta? (valor da aposta atual: R$ " + apostaDaRodada + ")");
                     decisaoJogador[i][1] = sc.nextInt();
-                    apostaDaRodada = (int) decisaoJogador[i][1];
+                    if(checkAposta(apostaDaRodada,(int)decisaoJogador[i][1])){
+                        apostaDaRodada = (int) decisaoJogador[i][1];
+                    }else {
+                        throw new ArithmeticException("opção inválida");
+                    }
+
                 } else {
                     decisaoJogador[i][0] = "out";
                     decisaoJogador[i][1] = 0;
                     jogadoresQueApostaram[i] = "out";
-                    Baralho.imprimeCartaJogadoresRestantes(cartasJogadores,jogadoresQueApostaram);
+                    Baralho.imprimeCartaJogadoresRestantes(cartasJogadores, jogadoresQueApostaram);
                 }
             } catch (Exception e) {
                 System.out.println("Jogador " + (i + 1) + " digitou uma opção inválida. Está fora da rodada");
                 decisaoJogador[i][0] = "out";
                 jogadoresQueApostaram[i] = "out";
-                Baralho.imprimeCartaJogadoresRestantes(cartasJogadores,jogadoresQueApostaram);
+                Baralho.imprimeCartaJogadoresRestantes(cartasJogadores, jogadoresQueApostaram);
                 decisaoJogador[i][1] = 0;
             }
         }
 
         //laço de repetição  verificando apostas
         do {
+            //verifico se todas as apostas são iguais
             for (int i = 0; i < decisaoJogador.length; i++) {
                 if (decisaoJogador[i][0] == "bet") {
-                    if ((int) decisaoJogador[i][1] >= apostaDaRodada) {
+                    if (checkAposta(apostaDaRodada,(int) decisaoJogador[i][1])){
                         apostaDaRodada = (int) decisaoJogador[i][1];
-                        apostasIguais = true;
+                        apostasIguais = true ;
                     } else {
-                        System.out.println("Jogador " + (i + 1) + ", a aposta atual é: " + apostaDaRodada + ". Você escolhe bet(1) ou out(2)?");
+                        apostasIguais = false;
+                        System.out.println("Jogador " + (i + 1) + ", a aposta atual é: R$" + apostaDaRodada);
+                        System.out.println("Você escolhe bet(1) ou out(2)?");
                         try {
                             opcaoJogador = sc.nextInt();
                             if (!(opcaoJogador == 1 || opcaoJogador == 2)) {
                                 throw new ArithmeticException("opção inválida");
                             } else if (opcaoJogador == 1) {
-                                System.out.println("Jogador escolheu bet. Qual o valor da aposta? (valor da aposta atual: " + apostaDaRodada+")");
+                                System.out.println("Jogador "+ (i+1) +" escolheu bet.");
+                                System.out.println("Qual o valor da aposta? (valor da aposta atual: " + apostaDaRodada + ")");
                                 decisaoJogador[i][1] = sc.nextInt();
-                                apostaDaRodada = (int) decisaoJogador[i][1];
+                                if (checkAposta(apostaDaRodada,(int) decisaoJogador[i][1])) {
+                                    apostaDaRodada = (int) decisaoJogador[i][1];
+                                } else {
+                                    throw new ArithmeticException("opção inválida");
+                                }
                             } else {
                                 decisaoJogador[i][0] = "out";
                                 jogadoresQueApostaram[i] = "out";
@@ -74,46 +88,59 @@ public class Rodadas {
                             }
 
                         } catch (Exception e) {
-                            System.out.println("Jogador " + (i + 1) + " digitou uma opção inválida. Está fora da rodada");
+                            System.out.println("Jogador " + (i + 1) + " digitou uma opção inválida . Está fora da rodada");
                             decisaoJogador[i][0] = "out";
                             jogadoresQueApostaram[i] = "out";
-                            Baralho.imprimeCartaJogadoresRestantes(cartasJogadores,jogadoresQueApostaram);
+                            Baralho.imprimeCartaJogadoresRestantes(cartasJogadores, jogadoresQueApostaram);
                         }
-
                     }
                 }
             }
-        } while (!apostasIguais);
+        }
+        while (!apostasIguais);
 
         // soma a rodada
-        for(int i = 0; i<decisaoJogador.length; i++){
+        for (int i = 0; i < decisaoJogador.length; i++) {
             somaApostas += (int) decisaoJogador[i][1];
         }
 
         return somaApostas;
     }
-        public int jogadoresDaRodada() {
-            boolean continua = false;
-            int jogadores=0;
-            //validador de jogadores para a rodada
-            while (!continua) {
-                System.out.println("quantos jogadores irão participar: ");
-                try {
-                    jogadores = sc.nextInt();
-                    if (jogadores <= 0) {
-                        throw new ArithmeticException("numero invalido");
-                    } else {
-                        continua = true;
-                    }
-                } catch (Exception e) {
-                    System.out.println("você digitou um número inválido de participantes");
-                }
-            }
-            return jogadores;
-        }
 
-        //getter p/ os jogadores que apostaram
+    public int jogadoresDaRodada() {
+        boolean continua = false;
+        int jogadores = 0;
+        //validador de jogadores para a rodada
+        while (!continua) {
+            System.out.println("quantos jogadores irão participar: ");
+            try {
+                jogadores = sc.nextInt();
+                if (jogadores <= 0) {
+                    throw new ArithmeticException("numero invalido");
+                } else {
+                    continua = true;
+                }
+            } catch (Exception e) {
+                System.out.println("você digitou um número inválido de participantes");
+            }
+        }
+        return jogadores;
+    }
+
+    //getter p/ os jogadores que apostaram
     public static String[] getJogadoresQueApostaram() {
         return jogadoresQueApostaram;
     }
+
+    public static boolean checkAposta(int apostaAtual, int apostaJogador) {
+
+        if (apostaJogador < apostaAtual) {
+            System.out.println("A aposta informada é menor do que a aposta atual, digite um valor maior ou igual ou peça pra sair.");
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
+
+
